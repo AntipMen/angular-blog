4b83f2e34a88ae1450755a6988909ef1b3632319
Anna <Anna.Antipova794gmail.com>
2021-03-14 20:13:51 +0200

    add authorization with firebase, auth.guard, auth.service

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -11,8 +13,14 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
   minLength = 6;
+  submitted = false;
+  message: string | undefined;
 
-  constructor() {
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
@@ -26,18 +34,32 @@ export class LoginPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params.loginAgain) {
+        this.message = 'Please, enter your details';
+      }
 
+    });
   }
 
   submit(): void {
-    console.log(this.form);
     if (this.form.invalid) {
       return;
     }
+    this.submitted = true;
 
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
     };
+
+    this.auth.login(user).subscribe(() => {
+        this.form.reset();
+        this.router.navigate(['/admin', 'dashboard']);
+        this.submitted = false;
+      },
+      () => {
+        this.submitted = false;
+      });
   }
 }
---
+++ src/app/admin/shared/services/auth.service.ts
@@ -0,0 +1,77 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, Subject, throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+
+import {FbAuthResponse, User} from '../../../shared/interfaces';
+import {environment} from '../../../../environments/environment';
+
+@Injectable()
+
+export class AuthService {
+
+  public error$: Subject<string> = new Subject<string>();
+
+  constructor(private http: HttpClient) {
+  }
+
+  get token(): string | null {
+    // @ts-ignore
+    const expDate = new Date(localStorage.getItem('fb-token-date'));
+    if (new Date() > expDate) {
+      this.logout();
+      return 'null';
+    }
+    return localStorage.getItem('fb-token');
+  }
+
+  login(user: User): Observable<any> {
+    user.returnSecureToken = true;
+
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        // @ts-ignore
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
+      );
+  }
+
+  logout(): void {
+    this.setToken(null);
+  }
+
+  isAuthenticated(): boolean {
+   if (this.token !== 'null') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error;
+    switch (message) {
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Email not found');
+        break;
+      case 'INVALID_EMAIL':
+        this.error$.next('Invalid Email');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Invalid Password');
+        break;
+    }
+    return throwError(error);
+  }
+
+  private setToken(response: FbAuthResponse | null): void {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-date', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
+
+  }
+}
